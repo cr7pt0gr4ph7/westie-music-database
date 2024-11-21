1fc91c897cf048d413246171a87fd122a248f9e5
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2024-11-21 22:04:09 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -203,7 +203,7 @@ if songs_together_toggle:
     st.markdown("#### Enter a partial/full `track.name` or `song_id`:")
     song_input = st.text_input("Song name:")
     song_input_prepped = song_input.lower().strip()
-    artist_input = st.text_input('Artist name:').lower()
+    artist_name_input = st.text_input("Artist's name:").lower()
     st.markdown(f"#### Most common songs played next to _{song_input}_:")
     st.text("Song name: song_id (to distinguish between song versions)")
     
@@ -236,7 +236,7 @@ if songs_together_toggle:
             pl.col('times_played_together').gt(1),
             )
      .filter(pl.col('pair').str.to_lowercase().str.contains(song_input_prepped),
-             pl.col('artist').str.to_lowercase().str.contains(artist_input))
+             pl.col('artist').str.to_lowercase().str.contains(artist_name_input))
      .with_columns(pl.col('pair').str.split(' --- '))
      .sort('times_played_together',
            pl.col('owner.display_name').list.len(), 
