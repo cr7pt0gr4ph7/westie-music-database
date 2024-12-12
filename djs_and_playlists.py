28c3ee22f48d34d3c31a1291a46b53a7c48e7eb5
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2024-12-12 19:43:37 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -149,6 +149,7 @@ if search_dj_toggle:
                               .list.unique()
                               .list.drop_nulls()
                               .list.sort()
+                              .list.head(50)
                               )
                 .sort(pl.col('playlist_name').list.len(), descending=True)
                 .head(50)
