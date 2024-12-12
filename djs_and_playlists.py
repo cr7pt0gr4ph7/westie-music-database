fa24ff3a931d45810c13e231b151cc6d89d781ab
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2024-12-12 20:16:35 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -87,7 +87,7 @@ if song_locator_toggle:
              pl.col('owner.display_name').str.to_lowercase().str.contains(dj_input))
      .group_by('track.name', 'track.id')
      .agg('playlist_name', 'owner.display_name', 'apprx_song_position_in_playlist', 'track.artists.id', 'track.artists.name', 'notes', 'note_source')
-     .with_columns(pl.col('playlist_name', 'owner.display_name', 'track.artists.id', 'track.artists.name').list.unique().list.sort(),
+     .with_columns(pl.col('playlist_name', 'owner.display_name', 'track.artists.id', 'track.artists.name').list.unique().list.drop_nulls().list.sort(),
                    pl.col('notes', 'note_source').list.unique().list.sort().list.drop_nulls())
      .sort(pl.col('playlist_name').list.len(), descending=True)
      .head(200).collect()
