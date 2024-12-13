1a4dad365dc9edd3c23f38655c5e6ba10932b108
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2024-12-13 19:41:39 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -419,7 +419,7 @@ if geo_region_toggle:
               .sort('dj_count', descending=True)
               )
     
-    st.dataframe(europe.head(100).collect(streaming=True))
+    st.dataframe(europe._fetch(100))
 
 
 
