7d86179778a4cc21c92f9ac52ffd76681cb606fc
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2024-12-26 23:54:07 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -480,8 +480,8 @@ if lyrics_toggle:
          .collect(streaming=True)
          .join(df.select('song_url', 
                         song = pl.col('track.name'), 
-                        artist = pl.col('track.artists.name')).unique(), 
-                how='left', on=['song', 'artist']).collect(streaming=True), 
+                        artist = pl.col('track.artists.name')).collect(streaming=True), 
+                how='left', on=['song', 'artist']), 
                  column_config={"song_url": st.column_config.LinkColumn()}
          )
 
