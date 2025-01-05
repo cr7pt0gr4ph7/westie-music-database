f70ee984782ace8374fa29ca7db5d4519daffafb
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-01-05 18:58:34 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -495,7 +495,8 @@ if country_region_toggle:
                                         'dj_count', 'playlist_count', 'country', 'geographic_region_count']
                                         )
                         .sort(['dj_count', 'playlist_count'], descending=True)
-                        .head(100).collect(streaming=True) , 
+                        # .head(100).collect(streaming=True) ,
+                        ._fetch(10000) 
                         column_config={"song_url": st.column_config.LinkColumn()})
 
 
