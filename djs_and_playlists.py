71405b67b14645db3375ab10678e05432eb232dc
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-01-05 01:12:18 +0100

    Add files via upload

--- djs_and_playlists.py
+++ djs_and_playlists.py
@@ -439,7 +439,6 @@ if geo_region_toggle:
                  .collect(streaming=True)
     )
     regions = ['Europe', 'North America', 'MENA', 'Oceania', 'Asia']
-    countries = df.select('country').unique().collect(streaming=True)['country'].to_list()
     region_selectbox = st.selectbox("Which Geographic Region would you like to see?",
                                     regions)
 
@@ -464,6 +463,9 @@ if country_region_toggle:
     st.markdown(f"#### Compare Countries' music:")
     country_1_selectbox = st.selectbox("Compare this country's music:", countries)
     country_2_selectbox = st.selectbox("To this country's music:", countries)
+    
+    regions = ['Europe', 'North America', 'MENA', 'Oceania', 'Asia']
+    countries = df.select('country').unique().collect(streaming=True)['country'].to_list()
     
     country_1_df = (df
               .filter(pl.col('region') == country_1_selectbox)
