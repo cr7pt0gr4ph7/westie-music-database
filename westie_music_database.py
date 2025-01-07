1e826af1dfa461e56e18fcfa0f303564685882d9
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-01-08 00:51:46 +0100

    Add files via upload

--- westie_music_database.py
+++ westie_music_database.py
@@ -145,6 +145,7 @@ if song_locator_toggle:
                                 pl.col('notes', 'note_source').list.unique().list.sort().list.drop_nulls(),
                                 hit_terms = pl.col('playlist_name')
                                                 .list.join(', ')
+                                                .str.to_lowercase()
                                                 .str.extract_many(playlist_input, ascii_case_insensitive=True)
                                                 .list.drop_nulls()
                                                 .list.unique()
