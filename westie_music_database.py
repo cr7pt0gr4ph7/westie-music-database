bb896995cf082d0e2ec78ac56f9549ab5f2d1353
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-01-16 15:24:05 +0100

    Add files via upload

--- westie_music_database.py
+++ westie_music_database.py
@@ -195,16 +195,10 @@ if song_locator_toggle:
         # countries_selectbox = st.multiselect("Country:", countries)
         # added_2_playlist_date = st.text_input("Added to playlist date (yyyy-mm-dd):")
         # track_release_date = st.text_input("Track release date (yyyy-mm-dd or '198' for 1980's music):")
-        
-        if all(not var for var in [song_input, artist_name, dj_input, playlist_input,
-                                 countries_selectbox, added_2_playlist_date, track_release_date]):
-                st.text('Nothing selected')
-                st.dataframe(top_songs(), 
-                                #  column_config={"song_url": st.column_config.LinkColumn()}
-                            )
 
-        elif any(var for var in [song_input, artist_name, dj_input, playlist_input,
+        if any(var for var in [song_input, artist_name, dj_input, playlist_input,
                                  countries_selectbox, added_2_playlist_date, track_release_date]):
+                st.text('something selected')
                 st.dataframe(df
                         .join(df_notes,
                                 how='full',
@@ -245,6 +239,13 @@ if song_locator_toggle:
                         .head(1000).collect(streaming=True), 
                         column_config={"song_url": st.column_config.LinkColumn()}
                         )
+        else:
+                # all(not var for var in [song_input, artist_name, dj_input, playlist_input,
+                #                  countries_selectbox, added_2_playlist_date, track_release_date]):
+                st.text('Nothing selected')
+                st.dataframe(top_songs(), 
+                                #  column_config={"song_url": st.column_config.LinkColumn()}
+                            )
         st.markdown(f"#### ")
 
 #courtesy of Vishal S
@@ -264,6 +265,7 @@ if playlist_locator_toggle:
                  column_config={"playlist_url": st.column_config.LinkColumn()}
                 )
         st.markdown(f"#### ")
+        
 
 
 
