87f25773b9d93cefb386b3a52f6ac7c188ef1ee8
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-02-02 00:06:46 +0100

    Add files via upload

--- westie_music_database.py
+++ westie_music_database.py
@@ -973,7 +973,7 @@ if lyrics_toggle:
 
 
 
-st.markdown("#### ")
+st.markdown("# ")
 # st.link_button('Follow me so I can add you to the database!',
 #                'https://open.spotify.com/user/225x7krl3utkpzg34gw3lhycy')
 st.link_button('📍 Find a WCS class near you!',
