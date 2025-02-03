ee3c3590011500971f25c0fcaaada9caac5d8697
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-02-03 23:00:14 +0100

    Add files via upload

--- westie_music_database.py
+++ westie_music_database.py
@@ -136,11 +136,11 @@ def load_playlist_data():
       .with_columns(pl.col('song_number', 'tracks.total').cast(pl.UInt16),
                     pl.col('geographic_region_count').cast(pl.Int8),
                     pl.col(['song_url', 'playlist_url', 'owner_url', 'song_position_in_playlist', 'apprx_song_position_in_playlist',
-                        #     'location',
-                        #     'region', 
-                        #     'country', 
-                        #     'playlist_name', 
-                        #     'owner.display_name',
+                            'location',
+                            'region', 
+                            'country', 
+                            'playlist_name', 
+                            'owner.display_name',
                             ]).cast(pl.Categorical())
                     )
 )
