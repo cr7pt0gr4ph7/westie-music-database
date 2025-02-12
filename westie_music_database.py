07902addbbf9fe8bd8e7f2882667f157611c42b1
Thomas <33540039+ThomasMAhern@users.noreply.github.com>
2025-02-12 22:25:34 +0100

    Add files via upload

--- westie_music_database.py
+++ westie_music_database.py
@@ -512,13 +512,15 @@ if song_locator_toggle:
                         )
                 
                 
-                playlists_text = ' '.join(' '.join(song_search_df
+                playlists_text = ' '.join(song_search_df
                                         .select(pl.col('playlist_name').cast(pl.List(pl.String)))
+                                        .explode('playlist_name')
+                                        .with_columns(pl.col('playlist_name').str.split(' '))
                                         .explode('playlist_name')
                                         .collect(streaming=True)
                                         ['playlist_name']
                                         .to_list()
-                                        ).lower().split(' '))
+                                        ).lower()
                 
                 # Generate the WordCloud
                 if playlists_text:
