3dec691e5ad1ee67c96c2da3d7834d61fda82944
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-03 00:55:45 +0200

    Update westie_music_database.py

--- westie_music_database.py
+++ westie_music_database.py
@@ -538,10 +538,10 @@ if song_locator_toggle:
         # st.write(countries)
         # st.write(countries_selectbox)
         
-        if not countries_selectbox:
-                countries_selectbox = ['Fantasia']
-        if countries_selectbox:
-                countries_selectbox = [c for c in countries if c not in countries_selectbox]
+        # if not countries_selectbox:
+        #         countries_selectbox = ['Fantasia']
+        # elif countries_selectbox:
+        #         countries_selectbox = [c for c in countries if c not in countries_selectbox]
         st.write(countries_selectbox)
                 
         
