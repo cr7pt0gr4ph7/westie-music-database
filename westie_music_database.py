8a06b2442d992b01f24b962ad51264d0743a6616
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-07 00:06:34 +0200

    Update westie_music_database.py

--- westie_music_database.py
+++ westie_music_database.py
@@ -301,10 +301,10 @@ def load_playlist_data():
       #memory tricks
       .with_columns(pl.col('song_number', 'tracks.total').cast(pl.UInt16),
                     pl.col('geographic_region_count').cast(pl.Int8),
-                    pl.col(['song_url', 'playlist_url', 'owner_url', 'song_position_in_playlist', 
+                    pl.col(['song_url', 'playlist_url', 'playlist_id', 'owner_url', 'song_position_in_playlist', 
                         #     'apprx_song_position_in_playlist',
                             'location','region', 'country', 'playlist_name', 'owner.display_name',
-                            'owner.id',
+                            'owner.id', 
                             ]).cast(pl.Categorical())
                     )
 )
