da264b3c1bf69b5cf9713e10a41cd09997f21711
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-07 00:43:39 +0200

    Update westie_music_database.py

--- westie_music_database.py
+++ westie_music_database.py
@@ -481,7 +481,7 @@ def top_songs():
                         how='full',
                         on=['track.artists.name', 'track.name'])
                 #add bpm
-                .join(pl.scan_parquet('data_song_bpm.parquet'), how='left', on=[pl.col('track.name'), pl.col('track.artists.name').cast(pl.Categorical)])
+                .join(pl.scan_parquet('data_song_bpm.parquet').with_columns(pl.col('track.artists.name').cast(pl.String)), how='left', on=['track.name', 'track.artists.name'])
                 
                 .with_columns(pl.col('bpm').fill_null(pl.col('BPM')))
                 .group_by('track.name', 'song_url', 'playlist_count', 'dj_count')
