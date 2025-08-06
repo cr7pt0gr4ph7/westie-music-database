386600e62f2583a987d83959f180e8514d4cca08
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-07 00:54:41 +0200

    Update westie_music_database.py

--- westie_music_database.py
+++ westie_music_database.py
@@ -342,7 +342,7 @@ def load_stats():
         '''makes it so streamlit doesn't have to reload for every sesson/updated parameter
         should make it much more responsive'''
         stats_counts = (pl.scan_parquet('data_playlists.parquet')
-                        pl.col('track.artists.name').cast(pl.Categorical)
+                        .with_columns(pl.col('track.artists.name').cast(pl.Categorical))
                         .select(pl.n_unique('track.name'),
                                 pl.n_unique('track.artists.name'),
                                 pl.n_unique('name'),
