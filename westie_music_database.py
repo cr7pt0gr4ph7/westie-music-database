029e509bb36a183142c1d476ae20269a14038194
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2025-08-19 21:38:02 +0200

    Update westie_music_database.py

--- westie_music_database.py
+++ westie_music_database.py
@@ -608,6 +608,7 @@ if song_locator_toggle:
         # else:
         if st.button("Search songs", type="primary", disabled=st.session_state["processing"]) and (st.session_state["processing"] == False):
                 st.session_state["processing"] = True
+                st.write(st.session_state["processing"])
                 log_query("Search songs", {'song_input': song_input,
                                            'artist_name': artist_name,
                                            'dj_input': dj_input,
