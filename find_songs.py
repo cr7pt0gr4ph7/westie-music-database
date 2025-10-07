af933b5fc8c604dd4cfa2ac09f3c17a5abea883b
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-10-07 12:46:51 +0000

    [find_songs] wip/preprocess: Include BPM values in pre-processed Parquet files

--- find_songs.py
+++ find_songs.py
@@ -10,10 +10,17 @@ if len(sys.argv) >= 2:
 else:
     mode = 'load'
 
+TRACK_ID_DTYPE = pl.String
+TRACK_BPM_DTYPE = pl.UInt8
+TRACK_NAME_DTYPE = pl.String
+TRACK_ARTIST_DTYPE = pl.String
+PLAYLIST_ID_DTYPE = pl.String
+
 # Handle different caching modes
 if mode == 'live' or mode == 'write':
     # Calculate the derived data at runtime, and optionally write it to a file
     source_data = pl.scan_parquet('data_playlists.parquet')
+    bpm_data = pl.scan_parquet('data_song_bpm.parquet')
 
     playlists = source_data.select(
         pl.col('playlist_id').alias('playlist.id'),
@@ -53,9 +60,9 @@ if mode == 'live' or mode == 'write':
         playlists_extended.sink_parquet('data_playlist_metadata.parquet')
 
     tracks = source_data.select(
-        pl.col('track.id').alias('track.id'),
-        pl.col('track.name'),
-        pl.col('track.artists.name'),
+        pl.col('track.id').cast(TRACK_ID_DTYPE).alias('track.id'),
+        pl.col('track.name').cast(TRACK_NAME_DTYPE),
+        pl.col('track.artists.name').cast(TRACK_ARTIST_DTYPE),
         pl.col('track.album.release_date'),
     ).sort('track.id').unique()
 
@@ -64,15 +71,20 @@ if mode == 'live' or mode == 'write':
             queer_artists).alias("track.artists.is_queer_artist"),
         pl.col('track.artists.name').str.to_lowercase().is_in(
             poc_artists).alias("track.artists.is_poc_artist"),
-    )
+    ).join(
+        bpm_data.select(
+            pl.col('track.name').cast(TRACK_NAME_DTYPE),
+            pl.col('track.artists.name').cast(TRACK_ARTIST_DTYPE),
+            pl.col('bpm').cast(TRACK_BPM_DTYPE).alias('track.bpm')),
+        how='left', on=['track.name', 'track.artists.name'])
 
     # Write pre-processed track data to file
     if mode == 'write':
         tracks_extended.sink_parquet('data_song_metadata.parquet')
 
     playlist_tracks = source_data.select(
-        pl.col('playlist_id').alias('playlist.id'),
-        pl.col('track.id').alias('track.id'),
+        pl.col('playlist_id').cast(PLAYLIST_ID_DTYPE).alias('playlist.id'),
+        pl.col('track.id').cast(TRACK_ID_DTYPE).alias('track.id'),
         # The following metadata is not strictly required
         pl.col('song_number').alias('playlist_track.number'),
         pl.col('added_at').alias('playlist_track.added_at'),
