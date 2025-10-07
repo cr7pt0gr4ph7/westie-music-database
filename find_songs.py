157c2f945179fab901f4cd130cbc9eb56f2a812c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-10-07 10:50:27 +0000

    [find_songs] wip/preprocess: Extract data from playlists

--- find_songs.py
+++ find_songs.py
@@ -1,4 +1,5 @@
 import polars as pl
+import polars.selectors as cs
 
 source_data = pl.scan_parquet('data_playlists.parquet')
 
@@ -6,10 +7,20 @@ playlists = source_data.select(
     pl.col('playlist_id').alias('playlist.id'),
     pl.col('name').alias('playlist.name'),
     pl.col('owner.id'),
-    pl.col('owner.display_name').alias('owner.name')
+    pl.col('owner.display_name').alias('owner.name'),
+    # Only required for extended data below
+    pl.col('location').alias('playlist.location'),
 ).unique()
 
-q = playlists
+from utils.playlist_classifiers import extract_dates_from_name
+
+playlists_extended = playlists.with_columns(
+    extract_dates_from_name(pl.col('playlist.name')).cast(pl.List(pl.Categorical)).alias('playlist.extracted_date'),
+    pl.col('playlist.location').str.split(' - ').list.get(0, null_on_oob=True).alias('playlist.region'),
+    pl.col('playlist.location').str.split(' - ').list.get(1, null_on_oob=True).alias('playlist.country')
+).select(cs.all() - cs.by_name('playlist.location'))
+
+q = playlists_extended
 
 print(q.limit(50).collect())
 
