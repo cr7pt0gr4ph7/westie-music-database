18ef90a2091a058b50dc4965bb2a63989268162c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-10-07 11:27:17 +0000

    [find_songs] wip/preprocess: Add an option to load existing datasets

--- find_songs.py
+++ find_songs.py
@@ -4,67 +4,80 @@ import polars.selectors as cs
 from utils.playlist_classifiers import extract_dates_from_name
 from utils.additional_data import actual_wcs_djs
 
-source_data = pl.scan_parquet('data_playlists.parquet')
+mode = 'load'
 
-playlists = source_data.select(
-    pl.col('playlist_id').alias('playlist.id'),
-    pl.col('name').alias('playlist.name'),
-    pl.col('owner.id'),
-    pl.col('owner.display_name').alias('owner.name'),
-    # Only required for extended data below
-    pl.col('location').alias('playlist.location'),
-).unique()
+# Handle different caching modes
+if mode == 'live' or mode == 'write':
+    # Calculate the derived data at runtime, and optionally write it to a file
+    source_data = pl.scan_parquet('data_playlists.parquet')
 
-_is_social_set = (
-    pl.col('playlist.extracted_date').list.len().gt(0)
-    | pl.col('playlist.name').str.contains_any(['social', 'party', 'soir'], ascii_case_insensitive=True)
-)
+    playlists = source_data.select(
+        pl.col('playlist_id').alias('playlist.id'),
+        pl.col('name').alias('playlist.name'),
+        pl.col('owner.id'),
+        pl.col('owner.display_name').alias('owner.name'),
+        # Only required for extended data below
+        pl.col('location').alias('playlist.location'),
+    ).unique()
 
-_is_wcs_dj = (
-    pl.col('owner.id').str.contains_any(
-        actual_wcs_djs, ascii_case_insensitive=True)
-    | pl.col('owner.name').cast(pl.String).eq('Connie Wang')
-    | pl.col('owner.name').cast(pl.String).eq('Koichi Tsunoda')
-)
+    _is_social_set = (
+        pl.col('playlist.extracted_date').list.len().gt(0)
+        | pl.col('playlist.name').str.contains_any(['social', 'party', 'soir'], ascii_case_insensitive=True)
+    )
 
-playlists_extended = playlists.with_columns(
-    extract_dates_from_name(pl.col('playlist.name')).cast(
-        pl.List(pl.Categorical)).alias('playlist.extracted_date'),
-    pl.col('playlist.location').str.split(' - ').list.get(
-        0, null_on_oob=True).alias('playlist.region'),
-    pl.col('playlist.location').str.split(' - ').list.get(
-        1, null_on_oob=True).alias('playlist.country'),
-).with_columns(
-    _is_social_set.alias('playlist.is_social_set'),
-    _is_wcs_dj.alias('owner.is_wcs_dj'),
-).select(cs.all() - cs.by_name('playlist.location'))
+    _is_wcs_dj = (
+        pl.col('owner.id').str.contains_any(
+            actual_wcs_djs, ascii_case_insensitive=True)
+        | pl.col('owner.name').cast(pl.String).eq('Connie Wang')
+        | pl.col('owner.name').cast(pl.String).eq('Koichi Tsunoda')
+    )
 
-# Write pre-processed playlist data to file
-playlists_extended.collect().write_parquet('data_playlist_metadata.parquet')
+    playlists_extended = playlists.with_columns(
+        extract_dates_from_name(pl.col('playlist.name')).cast(
+            pl.List(pl.Categorical)).alias('playlist.extracted_date'),
+        pl.col('playlist.location').str.split(' - ').list.get(
+            0, null_on_oob=True).alias('playlist.region'),
+        pl.col('playlist.location').str.split(' - ').list.get(
+            1, null_on_oob=True).alias('playlist.country'),
+    ).with_columns(
+        _is_social_set.alias('playlist.is_social_set'),
+        _is_wcs_dj.alias('owner.is_wcs_dj'),
+    ).select(cs.all() - cs.by_name('playlist.location'))
+
+    # Write pre-processed playlist data to file
+    if mode == 'write':
+        playlists_extended.collect().write_parquet('data_playlist_metadata.parquet')
+
+    tracks = source_data.select(
+        pl.col('track.id').alias('track.id'),
+        pl.col('track.name'),
+        pl.col('track.artists.name'),
+        pl.col('track.album.release_date'),
+    ).unique()
+
+    # Write pre-processed track data to file
+    if mode == 'write':
+        tracks.collect().write_parquet('data_song_metadata.parquet')
+
+    playlist_tracks = source_data.select(
+        pl.col('playlist_id').alias('playlist.id'),
+        pl.col('track.id').alias('track.id'),
+        # The following metadata is not strictly required
+        pl.col('song_number').alias('playlist_track.number'),
+        pl.col('added_at').alias('playlist_track.added_at'),
+    )
+
+    # Write pre-processed track <=> playlist membership data to file
+    if mode == 'write':
+        tracks.collect().write_parquet('data_playlist_songs.parquet')
+
+elif mode == 'load':
+    # Load the pre-generated data from the Parquet files
+    playlists_extended = playlists = pl.scan_parquet(
+        'data_playlist_metadata.parquet')
+    playlist_tracks = pl.scan_parquet('data_playlist_songs.parquet')
+    tracks = pl.scan_parquet('data_song_metadata.parquet')
 
 q = playlists_extended
 
 print(q.limit(50).collect())
-
-print(source_data.schema)
-
-tracks = source_data.select(
-    pl.col('track.id').alias('track.id'),
-    pl.col('track.name'),
-    pl.col('track.artists.name'),
-    pl.col('track.album.release_date'),
-).unique()
-
-# Write pre-processed track data to file
-tracks.collect().write_parquet('data_song_metadata.parquet')
-
-playlist_tracks = source_data.select(
-    pl.col('playlist_id').alias('playlist.id'),
-    pl.col('track.id').alias('track.id'),
-    # The following metadata is not strictly required
-    pl.col('song_number').alias('playlist_track.number'),
-    pl.col('added_at').alias('playlist_track.added_at'),
-)
-
-# Write pre-processed track <=> playlist membership data to file
-tracks.collect().write_parquet('data_playlist_songs.parquet')
