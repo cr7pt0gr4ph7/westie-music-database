df1d9bbed5377b91c0787dbc9518f7dcf5790fe4
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-10-07 11:06:37 +0000

    [find_songs] wip/preprocess: Compute additional data for playlists based on heuristics

--- find_songs.py
+++ find_songs.py
@@ -1,6 +1,9 @@
 import polars as pl
 import polars.selectors as cs
 
+from utils.playlist_classifiers import extract_dates_from_name
+from utils.additional_data import actual_wcs_djs
+
 source_data = pl.scan_parquet('data_playlists.parquet')
 
 playlists = source_data.select(
@@ -12,12 +15,28 @@ playlists = source_data.select(
     pl.col('location').alias('playlist.location'),
 ).unique()
 
-from utils.playlist_classifiers import extract_dates_from_name
+_is_social_set = (
+    pl.col('playlist.extracted_date').list.len().gt(0)
+    | pl.col('playlist.name').str.contains_any(['social', 'party', 'soir'], ascii_case_insensitive=True)
+)
+
+_is_wcs_dj = (
+    pl.col('owner.id').str.contains_any(
+        actual_wcs_djs, ascii_case_insensitive=True)
+    | pl.col('owner.name').cast(pl.String).eq('Connie Wang')
+    | pl.col('owner.name').cast(pl.String).eq('Koichi Tsunoda')
+)
 
 playlists_extended = playlists.with_columns(
-    extract_dates_from_name(pl.col('playlist.name')).cast(pl.List(pl.Categorical)).alias('playlist.extracted_date'),
-    pl.col('playlist.location').str.split(' - ').list.get(0, null_on_oob=True).alias('playlist.region'),
-    pl.col('playlist.location').str.split(' - ').list.get(1, null_on_oob=True).alias('playlist.country')
+    extract_dates_from_name(pl.col('playlist.name')).cast(
+        pl.List(pl.Categorical)).alias('playlist.extracted_date'),
+    pl.col('playlist.location').str.split(' - ').list.get(
+        0, null_on_oob=True).alias('playlist.region'),
+    pl.col('playlist.location').str.split(' - ').list.get(
+        1, null_on_oob=True).alias('playlist.country'),
+).with_columns(
+    _is_social_set.alias('playlist.is_social_set'),
+    _is_wcs_dj.alias('owner.is_wcs_dj'),
 ).select(cs.all() - cs.by_name('playlist.location'))
 
 q = playlists_extended
