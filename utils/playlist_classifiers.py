1e8657cdc1fef1665e84d72b692acfa41ad12808
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-10-24 12:18:31 +0000

    [date] refactor: Restructure the date extraction logic

--- utils/playlist_classifiers.py
+++ utils/playlist_classifiers.py
@@ -1,5 +1,6 @@
 """Utilities for extracting calendar dates and BPM ranges from playlist names."""
 
+from typing import NamedTuple
 import polars as pl
 
 from utils.keyword_data import load_keyword_aliases
@@ -50,64 +51,85 @@ def extract_tags_from_name(expr: pl.Expr) -> pl.Expr:
 # Patterns for detecting calendar dates in playlist names #
 ###########################################################
 
-pattern_yyyy_mm_dd = r'\b(?:19|20)\d{2}[-/.](?:0[1-9]|1[0-2])[-/.](?:0[1-9]|[12]\d|3[01])\b'
-pattern_yyyy_dd_mm = r'\b(?:19|20)\d{2}[-/.](?:0[1-9]|[12]\d|3[01])[-/.](?:0[1-9]|1[0-2])\b'
-pattern_dd_mm_yyyy = r'\b(?:0[1-9]|[12]\d|3[01])[-/.](?:0[1-9]|1[0-2])[-/.](?:19|20)\d{2}\b'
-pattern_mm_dd_yyyy = r'\b(?:0[1-9]|1[0-2])[-/.](?:0[1-9]|[12]\d|3[01])[-/.](?:19|20)\d{2}\b'
 
-pattern_yy_mm_dd = r'\b\d{2}[-/.](?:0[1-9]|1[0-2])[-/.](?:0[1-9]|[12]\d|3[01])\b'
-pattern_yy_dd_mm = r'\b\d{2}[-/.](?:0[1-9]|[12]\d|3[01])[-/.](?:0[1-9]|1[0-2])\b'
-pattern_dd_mm_yy = r'\b(?:0[1-9]|[12]\d|3[01])[-/.](?:0[1-9]|1[0-2])[-/.]\d{2}\b'
-pattern_mm_dd_yy = r'\b(?:0[1-9]|1[0-2])[-/.](?:0[1-9]|[12]\d|3[01])[-/.]\d{2}\b'
+def date_part(name: str, pattern: str):
+    return f'(?<{name}>{pattern})'
 
-pattern_dd_MMM_yyyy = r'\b(?:0[1-9]|[12]\d|3[01])[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?(?:19|20)\d{2}\b'
-pattern_MMM_dd_yyyy = r'\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?(?:0[1-9]|[12]\d|3[01])[-/. ]?(?:19|20)\d{2}\b'
-pattern_yyyy_MMM_dd = r'\b(?:19|20)\d{2}[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?(?:0[1-9]|[12]\d|3[01])\b'
-pattern_yyyy_dd_MMM = r'\b(?:19|20)\d{2}[-/. ]?(?:0[1-9]|[12]\d|3[01])[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b'
 
-pattern_dd_MMM_yy = r'\b(?:0[1-9]|[12]\d|3[01])[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?\d{2}\b'
-pattern_MMM_dd_yy = r'\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?(?:0[1-9]|[12]\d|3[01])[-/. ]?\d{2}\b'
-pattern_yy_MMM_dd = r'\b\d{2}[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[-/. ]?(?:0[1-9]|[12]\d|3[01])\b'
-pattern_yy_dd_MMM = r'\b\d{2}[-/. ]?(?:0[1-9]|[12]\d|3[01])[-/. ]?(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b'
+yy = date_part('year', r'\d{2}')
+yyyy = date_part('year', r'(?:19|20)\d{2}')
+mm = date_part('month', r'(?:0[1-9]|1[0-2])')
+MMM = date_part('month', r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)')
+dd = date_part('day', r'(?:0[1-9]|[12]\d|3[01])')
 
-pattern_mm_yy = r'\b(?:0[1-9]|1[0-2])[-/. ]\d{2}\b'
-pattern_dd_mm = r'\b(?:0[1-9]|[12]\d|3[01])[-/. ](?:0[1-9]|1[0-2])\b'
-pattern_yy_mm = r'\b\d{2}[-/. ](?:0[1-9]|1[0-2])\b'
-pattern_mm_dd = r'\b(?:0[1-9]|1[0-2])[-/. ](?:0[1-9]|[12]\d|3[01])\b'
 
-pattern_month_year_or_reversed = r"\b(?:(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* \d{4}|\d{4} (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*)\b"
+def date_pattern(name: str, pattern: str):
+    return f'\\b{pattern}\\b'
+
+
+pattern_yyyy_mm_dd = date_pattern('yyyy_mm_dd', f'{yyyy}[-/.]{mm}[-/.]{dd}')
+pattern_yyyy_dd_mm = date_pattern('yyyy_dd_mm', f'{yyyy}[-/.]{dd}[-/.]{mm}')
+pattern_dd_mm_yyyy = date_pattern('dd_mm_yyyy', f'{dd}[-/.]{mm}[-/.]{yyyy}')
+pattern_mm_dd_yyyy = date_pattern('mm_dd_yyyy', f'{mm}[-/.]{dd}[-/.]{yyyy}')
+
+pattern_yy_mm_dd = date_pattern('yy_mm_dd', f'{yy}[-/.]{mm}[-/.]{dd}')
+pattern_yy_dd_mm = date_pattern('yy_dd_mm', f'{yy}[-/.]{dd}[-/.]{mm}')
+pattern_dd_mm_yy = date_pattern('dd_mm_yy', f'{dd}[-/.]{mm}[-/.]{yy}')
+pattern_mm_dd_yy = date_pattern('mm_dd_yy', f'{mm}[-/.]{dd}[-/.]{yy}')
+
+pattern_dd_MMM_yyyy = date_pattern('dd_MMM_yyyy', f'{dd}[-/. ]?{MMM}[-/. ]?{yyyy}')
+pattern_MMM_dd_yyyy = date_pattern('MMM_dd_yyyy', f'{MMM}[-/. ]?{dd}[-/. ]?{yyyy}')
+pattern_yyyy_MMM_dd = date_pattern('yyyy_MMM_dd', f'{yyyy}[-/. ]?{MMM}[-/. ]?{dd}')
+pattern_yyyy_dd_MMM = date_pattern('yyyy_dd_MMM', f'{yyyy}[-/. ]?{dd}[-/. ]?{MMM}')
+
+pattern_dd_MMM_yy = date_pattern('dd_MMM_yy', f'{dd}[-/. ]?{MMM}[-/. ]?{yy}')
+pattern_MMM_dd_yy = date_pattern('MMM_dd_yy', f'{MMM}[-/. ]?{dd}[-/. ]?{yy}')
+pattern_yy_MMM_dd = date_pattern('yy_MMM_dd', f'{yy}[-/. ]?{MMM}[-/. ]?{dd}')
+pattern_yy_dd_MMM = date_pattern('yy_dd_MMM', f'{yy}[-/. ]?{dd}[-/. ]?{MMM}')
+
+pattern_mm_yy = date_pattern('mm_yy', f'{mm}[-/. ]{yy}')
+pattern_dd_mm = date_pattern('dd_mm', f'{dd}[-/. ]{mm}')
+pattern_yy_mm = date_pattern('yy_mm', f'{yy}[-/. ]{mm}')
+pattern_mm_dd = date_pattern('mm_dd', f'{mm}[-/. ]{dd}')
+
+pattern_month_year_or_reversed = date_pattern('month_year_or_reversed', f'(?:{MMM}[a-z]* {yyyy}|{yyyy} {MMM}[a-z]*)')
+
+patterns_date = [
+    pattern_yyyy_mm_dd,
+    pattern_yyyy_dd_mm,
+    pattern_dd_mm_yyyy,
+    pattern_mm_dd_yyyy,
+
+    pattern_yy_mm_dd,
+    pattern_yy_dd_mm,
+    pattern_dd_mm_yy,
+    pattern_mm_dd_yy,
+
+    pattern_dd_MMM_yyyy,
+    pattern_MMM_dd_yyyy,
+    pattern_yyyy_MMM_dd,
+    pattern_yyyy_dd_MMM,
+
+    pattern_dd_MMM_yy,
+    pattern_yy_MMM_dd,
+    # pattern_MMM_dd_yy,  # matches on Jul 2024 as a date :(
+    # pattern_yy_dd_MMM,  # matches on 2024 Jul as a date :(
+
+    # pattern_mm_yy,
+    # pattern_dd_mm,
+    # pattern_yy_mm,
+    # pattern_mm_dd,
+]
 
 
 def extract_dates_from_name(playlist_name: pl.Expr, *, sort: bool = False):
     """"Extract a list of calendar dates from the given playlist name."""
-    result = pl.concat_list(
-        playlist_name.str.extract_all(pattern_yyyy_mm_dd),
-        playlist_name.str.extract_all(pattern_yyyy_dd_mm),
-        playlist_name.str.extract_all(pattern_dd_mm_yyyy),
-        playlist_name.str.extract_all(pattern_mm_dd_yyyy),
-
-        playlist_name.str.extract_all(pattern_yy_mm_dd),
-        playlist_name.str.extract_all(pattern_yy_dd_mm),
-        playlist_name.str.extract_all(pattern_dd_mm_yy),
-        playlist_name.str.extract_all(pattern_mm_dd_yy),
-
-        playlist_name.str.extract_all(pattern_dd_MMM_yyyy),
-        playlist_name.str.extract_all(pattern_MMM_dd_yyyy),
-        playlist_name.str.extract_all(pattern_yyyy_MMM_dd),
-        playlist_name.str.extract_all(pattern_yyyy_dd_MMM),
-
-        playlist_name.str.extract_all(pattern_dd_MMM_yy),
-        playlist_name.str.extract_all(pattern_yy_MMM_dd),
-        # playlist_name.str.extract_all(pattern_MMM_dd_yy), #matches on Jul 2024 as a date :(
-        # playlist_name.str.extract_all(pattern_yy_dd_MMM),  #matches on 2024 Jul as a date :(
-
-        # playlist_name.str.extract_all(pattern_mm_yy),
-        # playlist_name.str.extract_all(pattern_dd_mm),
-        # playlist_name.str.extract_all(pattern_yy_mm),
-        # playlist_name.str.extract_all(pattern_mm_dd),
-    ).list.unique()
+    result = pl.concat_list([
+        playlist_name.str.extract_all(date_pattern) for date_pattern in patterns_date
+    ]).list.unique()
 
     return result.list.sort() if sort else result
+
 
 #######################################################
 # Patterns for detecting BPM ranges in playlist names #
