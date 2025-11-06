7e4e5e0936a8a546057809156884454c6c9033b3
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-11-06 13:44:46 +0000

    [tags] fix: Skip loading the separate adjacent tags file

--- utils/search.py
+++ utils/search.py
@@ -1585,12 +1585,6 @@ class SearchEngine:
                 ~self.data.keywords.strip_from_tracks.matches(
                     pl.element().struct.field(TrackTag.tag))))\
             .with_columns(TrackTags.unnest_tags_data())\
-            .select(pl.exclude('adjacent_tags'))\
-            .join(pl.scan_parquet(DATA_DIR + 'data_song_adjacent_tags[min_size_10][-2,+2].parquet', glob=False)
-                  .select(Track.id, TrackTags.tags_data().alias('adjacent_tags_data')),
-                  how='left', on=Track.id)\
-            .with_columns(pl.col('adjacent_tags_data').fill_null(pl.lit([])))\
-            .sort_by(sort_by, descending=descending)\
             .included_tracks
 
     def find_playlists(
