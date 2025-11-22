57d80e685809b5bb66fe20f552d0d3b5dd8a22fa
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-11-22 00:18:27 +0000

    Revert "skip! [utils] feat: Add lazy_pivot (WIP)"
    This reverts commit d5d5fac823f26bb117c4d0d35ae5909bd0109011.

--- utils/common/polars.py
+++ utils/common/polars.py
@@ -1,12 +1,7 @@
-from collections.abc import Sequence
-from typing import Any, Literal
-
 import polars as pl
-import polars.selectors as cs
 
-type IntoExpr = str | list[str] | pl.Expr | cs.Selector
-type ColumnNameOrSelector = str | cs.Selector
-type PivotAgg = Literal["min", "max", "first", "last", "sum", "mean", "median", "len"]
+
+type IntoExpr = str | list[str] | pl.Expr
 
 
 def into_expr(expr: IntoExpr) -> pl.Expr:
@@ -14,61 +9,8 @@ def into_expr(expr: IntoExpr) -> pl.Expr:
         return pl.col(expr)
     if isinstance(expr, list):
         return pl.col(expr)
-    if cs.is_selector(expr):
-        return expr.as_expr()
     return expr
 
 
 def optional_alias(expr: pl.Expr, new_name: str | None) -> pl.Expr:
     return expr.alias(new_name) if new_name else expr
-
-
-def get_aggregate_expression(expr: pl.Expr, aggregate_function: PivotAgg | None):
-    match aggregate_function:
-        case "min":
-            return expr.min()
-        case "max":
-            return expr.max()
-        case "first":
-            return expr.first()
-        case "last":
-            return expr.last()
-        case "sum":
-            return expr.sum()
-        case "mean":
-            return expr.mean()
-        case "median":
-            return expr.median()
-        case "len":
-            return expr.len()
-        case None:
-            return expr.item(allow_empty=True)
-        case _:
-            raise ValueError(f"Invalid aggregate_function value: {aggregate_function}")
-
-
-def lazy_pivot(
-    lf: pl.LazyFrame,
-    on: ColumnNameOrSelector | Sequence[ColumnNameOrSelector],
-    unique_on_values: Sequence[Any],
-    *,
-    index: ColumnNameOrSelector | Sequence[ColumnNameOrSelector] | None = None,
-    values: ColumnNameOrSelector | Sequence[ColumnNameOrSelector] | None = None,
-    aggregate_function: PivotAgg | None = None,
-    maintain_order: bool = True,
-    sort_columns: bool = False,
-) -> pl.LazyFrame:
-    if sort_columns:
-        unique_on_values = list(unique_on_values)
-        unique_on_values.sort()
-
-    return lf.group_by(
-        into_expr(index),
-        maintain_order=maintain_order,
-    ).agg(
-        get_aggregate_expression(
-            into_expr(values).filter(on == value),
-            aggregate_function
-        ).alias(value)
-        for value in unique_on_values
-    ).collect()
